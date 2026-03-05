13957f056b63154265d8b9fc804613044b868fae
vidyanshumishra-byte <vidyanshumishra99@gmail.com>
2026-03-05 23:46:02 +0530

    Added JSON storage and task persistence

--- task_manager.py
+++ task_manager.py
@@ -1,31 +1,71 @@
-# Simple Task Manager - Version 1
+import json
 
-tasks = []
+try:
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+except:
+    tasks = []
 
+def save_tasks():
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file)
 def show_menu():
-    print("\n--- Task Manager ---")
+    print("\n--- Student Task Manager ---")
     print("1. Add Task")
     print("2. View Tasks")
-    print("3. Exit")
+    print("3. Delete Task")
+    print("4. Mark Complete")
+    print("5. Exit")
 
 while True:
     show_menu()
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        task = input("Enter task name: ")
-        tasks.append(task)
+        task = input("Enter task: ")
+        tasks.append({"task": task, "completed": False})
+        save_tasks()
         print("Task added successfully!")
 
     elif choice == "2":
         if len(tasks) == 0:
             print("No tasks available.")
         else:
-            print("\nYour Tasks:")
             for i in range(len(tasks)):
-                print(f"{i+1}. {tasks[i]}")
+                status = "✓" if tasks[i]["completed"] else "✗"
+                print(i + 1, ".", tasks[i]["task"], "-", status)
 
     elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, ".", tasks[i]["task"])
+
+            num = int(input("Enter task number to delete: "))
+            if 1 <= num <= len(tasks):
+                tasks.pop(num - 1)
+                save_tasks()
+                print("Task deleted successfully!")
+            else:
+                print("Invalid task number.")
+
+    elif choice == "4":
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, ".", tasks[i]["task"])
+
+            num = int(input("Enter task number to mark complete: "))
+            if 1 <= num <= len(tasks):
+                tasks[num - 1]["completed"] = True
+                save_tasks()
+                print("Task marked as complete!")
+            else:
+                print("Invalid task number.")
+
+    elif choice == "5":
         print("Exiting program...")
         break
 
